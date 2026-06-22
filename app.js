a50d7cd9a888f1e14395e858253567b3bfbb7ba2
Md Fahim <217421678+mdfahim10@users.noreply.github.com>
2026-06-23 02:16:16 +0530

    Added sessions, flash messaging, Passport.js configuration, user model, and signup authentication flow

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/wander_lust";
 
 
@@ -45,14 +51,26 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/",userRouter);
+
+
+
 // 404 Handler
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
