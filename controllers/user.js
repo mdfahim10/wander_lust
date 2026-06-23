e748cbb2bec11029216a871382a69052749c7cf2
Md Fahim <217421678+mdfahim10@users.noreply.github.com>
2026-06-24 01:35:19 +0530

    Implement MVC for listings, reviews, users

--- controllers/user.js
+++ controllers/user.js
@@ -9,8 +9,6 @@ module.exports.signup=async (req, res) => {
         let { username, email, password } = req.body;
         const newUser = new User({email,username,});
         const registeredUser = await User.register(newUser,password);
-        console.log(registeredUser);
-        
         req.login(registeredUser,(err)=>{
             if(err){
                 return next(err);
@@ -18,8 +16,6 @@ module.exports.signup=async (req, res) => {
             req.flash("success", "Welcome to WanderLust!");
             res.redirect("/listings");
         });
-
-        
     }catch(e){
         req.flash("error",e.message);
         res.redirect("/signup");
